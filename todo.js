71e8232b48219555c394dd65743e1123d0446b31
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-02-15 20:29:58 +0530

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="t.css">
+    
+   <div class="container">
+<h1>  MY TO DO LIST APP :)</h1>
+
+<div class="inputs">
+    <input type="text" placeholder="Enter something.." id="inp">
+    
+<button onclick="Add()"> Add</button>
+</div>
+   </div>
+<div class="text">
+
+</div>
+
+</head>
+
+<body>
+   <script src="t.js"></script> 
+</body>
+</html>
