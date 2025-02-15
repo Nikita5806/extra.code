23cdfb9036461bc10ac0e533947e4602f90f6be4
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-02-15 22:43:37 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -95,3 +95,59 @@ function Add(){
     }
    }
 }
+
+body{
+    background-color: rgb(49, 19, 49);
+    color: aliceblue;
+}
+.container{
+   width: 360px;
+   padding: 33px;
+   background-color: white;
+   color: rgb(73, 48, 71);
+   margin-left: 33%;
+   margin-top: 5px;
+
+   
+border: 4px solid rgb(196, 138, 169);
+  border-radius: 7px;
+  
+ 
+  
+}
+input{
+    width: 250px;
+    padding: 8px;
+    border: 3px solid pink;
+  
+}
+button{
+    width: 60px;
+    height: 40px;
+    background-color: rgb(237, 181, 218);
+    border-radius: 3px;
+    outline: none;
+    cursor: pointer;
+ 
+    
+}
+img{
+    height: 800px;
+    width: 500px;
+    margin-left: 900px;
+    margin-top: 7px;
+}
+.text ol {
+    position: relative;
+    background-color: rgb(175, 154, 167);
+    font-size: 16px;
+    text-align: left;
+    padding: 7px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+
+}
+.text ol i{
+    position: absolute;
+    right: 10px;
+    cursor: pointer;
+}
