5450bff06839138988fa5b00157ce6ff52eed205
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-03-24 09:43:05 +0530

    Create h.js

---
+++ h.js
@@ -0,0 +1,34 @@
+function maxProfit(prices) {
+    if (!prices.length) return 0;
+
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let price of prices) {
+        if (price < minPrice) {
+            minPrice = price;
+        } else if (price - minPrice > maxProfit) {
+            maxProfit = price - minPrice;
+        }
+    }
+
+    return maxProfit;
+}
+
+// Reading input from stdin
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+rl.on("line", (line) => {
+    input.push(line);
+}).on("close", () => {
+    const N = parseInt(input[0].trim(), 10);
+    const prices = input[1].trim().split(" ").map(Number);
+    
+    console.log(maxProfit(prices));
+});
