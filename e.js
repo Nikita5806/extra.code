268b9e5411b7dfada99f2c63a007528d1751a2fb
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-04-04 16:14:51 +0530

    Update e.js

--- e.js
+++ e.js
@@ -54,6 +54,19 @@ console.log(i)//24
  let j = --(i++); //  -- 12 , 11
 console.log(i)
 
+console.log(Math.round(10.4));// round of the value
+console.log(Math.ceil(10.2));// return decimal to higher value like 10.2 = 11
+console.log(Math.floor(10.9))// return decimal to lower value like 10.6 = 10
+console.log(Math.trunc(17.98)) // remove the decimal part
+console.log(Math.pow(2, 5)) //2* 5
+console.log(Math.sqrt(25)) // gives square root ie 5 
+console.log(Math.abs(-4)) //Convert negative to positive
+console.log(Math.max(5, 6, 44)) // gives max value from list 
+console.log(Math.min( 3 ,4 , 3 , 2)) // gives min val
+console.log(Math.trunc(Math.random()*9000)+ 1000)// gives 4 digit otp
+
+let a =  34.75834584678346;
+console.log(a.toFixed(5))
 
  
      
